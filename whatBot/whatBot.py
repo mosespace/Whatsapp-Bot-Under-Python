a55baa899b92b1dbbe6ade24d2d725adf3865890
Kisakye Moses <111237257+moses-kisakye@users.noreply.github.com>
2022-10-31 20:33:20 +0300

    Update whatBot.py

--- whatBot/whatBot.py
+++ whatBot/whatBot.py
@@ -3,6 +3,6 @@ import time
 time.sleep(4)
 count = 0
 while count <= 100:
-    pyautogui.typewrite("odangayo gaciya ")
+    pyautogui.typewrite("Place The Text You want To Send Here!")
     pyautogui.press("enter")
     
