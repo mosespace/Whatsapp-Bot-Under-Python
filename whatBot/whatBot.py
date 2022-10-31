55e288ec689338b5ecc3e52718668d7d3f9732bb
Kisakye Moses <111237257+moses-kisakye@users.noreply.github.com>
2022-10-31 21:00:52 +0300

    Update whatBot.py

--- whatBot/whatBot.py
+++ whatBot/whatBot.py
@@ -3,6 +3,7 @@ import time
 time.sleep(4)
 count = 0
 while count <= 100:
+# The <= 100: is the total number of messages to be sent. Your can change the value to any number of your choice!
     pyautogui.typewrite("Place The Text You want To Send Here!")
     pyautogui.press("enter")
     
